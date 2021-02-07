7bf20aba60ef8427f2caa688764c256a402719cd
Amanda Norman <amanda.j_n@hotmail.com>
2021-02-07 00:23:16 +0000

    Pause Menu and UI implementation
    Things added:
    
    --> Pause Menu: Pauses the game and shows a UI menu for the player to interact with
    
    --> Revamped the UI for menu, about screen and credits
    
    --> Added new title sprites
    
    --> Added background parallax for gameplay

---
+++ UnityGameJam2020/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        gamePaused = true;
+    }
+
+    public void Menu()
+    {
+        Debug.Log("Go to menu");
+    }
+
+    public void Exit()
+    {
+        Exit();
+    }
+}
