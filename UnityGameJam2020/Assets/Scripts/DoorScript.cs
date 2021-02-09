d8cbf523545ee114f077d883f36a568794778909
Amanda Norman <amanda.j_n@hotmail.com>
2021-02-09 16:42:37 +0000

    Level Connections
    Added level connections so that when the player reaches a door at the end of each level, it loads the next level. Also connected the menus and credits to the game. Added parallax to level 1.

--- UnityGameJam2020/Assets/Scripts/DoorScript.cs
+++ UnityGameJam2020/Assets/Scripts/DoorScript.cs
@@ -8,13 +8,18 @@ public class DoorScript : MonoBehaviour
     void OnTriggerStay2D(Collider2D collision)
     {
         string sceneName = SceneManager.GetActiveScene().name;
-        if (collision.gameObject.layer == 10)
+        if (collision.gameObject.tag == "Player")
         {
             if(Input.GetMouseButton(0))
             {
-                if (sceneName == "TestScene")
+                switch (sceneName)
                 {
-                    SceneManager.LoadScene("Credits");
+                    case "Level_1": SceneManager.LoadScene("Level_2"); break;
+                    case "Level_2": SceneManager.LoadScene("Level_3"); break;
+                    case "Level_3": SceneManager.LoadScene("Level_4"); break;
+                    case "Level_4": SceneManager.LoadScene("Level_5"); break;
+                    case "Level_5": SceneManager.LoadScene("Level_6"); break;
+                    case "Level_6": SceneManager.LoadScene("Credits"); break;
                 }
             }
         }
